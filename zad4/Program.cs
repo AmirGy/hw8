46f0e89f4bccf04829492ad02efd1299c29b0601
Amir <firstharrypotter1@gmail.com>
2022-12-24 21:25:51 +0600

    didnt solve zad4, but solved zad5

---
+++ zad4/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся (HARD - случайных уникальных) двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1) */
+
+void unique(int[,,] A)
+{
+    for (int x = 0; x < A.GetLength(0); x++)
+        for (int y = 0; y < A.GetLength(1); y++)
+        {
+            for (int z = 0; z < A.GetLength(2); z++)
+            {
+                A[x, y, z] = new Random().Next(10, 100);
+                Console.Write($"{A[x,y,z]}({x},{y},{z}) ");
+            }
+            Console.WriteLine();
+        }
+}
+
+Console.Write("Введите x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int[,,] A = new int[x,y,z];
+unique(A);
+
+
---
+++ zad5/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07 */
+
+int[,] SpiralArray(int[,] spiral, int x)
+{
+    int i = 0;
+    int j = 0;
+    for (int number = 1; number <= x * x; number++)
+    {
+        spiral[i, j] = number;
+        if (i <= j + 1 && i + j < x - 1)
+            j++;
+        else if (i < j && i + j >= x - 1)
+            i++;
+        else if (i >= j && i + j > x - 1)
+            j--;
+        else i--;
+    }
+    return spiral;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j], 2}\t ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int[,] spiral = new int[x, x];
+SpiralArray(spiral, x);
+PrintArray(spiral);
