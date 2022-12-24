8c57a10ffb38d5811784199ca25a5db67a6a9851
Amir <firstharrypotter1@gmail.com>
2022-12-24 18:15:56 +0600

    zad2 & zad3

---
+++ zad2/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
+
+int[,] CreateFillArray(int rows, int cols)
+{
+    int[,] array = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            array[i, j] = new Random().Next(0, 10);
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3}\t ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FindMinString(int[,] array)
+{
+    int[] SumString = new int[array.GetLength(0)];
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            sum += array[i,j];
+        SumString[i] = sum;
+        sum = 0;
+    }
+    int min = SumString[0];
+    for (int i = 0; i < SumString.Length; i++)
+        for (int j = i+1; j < SumString.Length; j++)
+            if(SumString[i] < SumString[j])
+                min = i + 1;
+    Console.Write($"Наименьшая сумма элементов в {min} строке.");
+}
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,] table = CreateFillArray(rows, cols);
+PrintArray(table);
+FindMinString(table);
+
---
+++ zad3/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+
+void MatrixProduct(int[,] A, int[,] B)
+{
+    int[,] C = new int[A.GetLength(0), B.GetLength(1)];
+    for (int i = 0; i < C.GetLength(0); i++)
+    {
+        for (int j = 0; j < C.GetLength(1); j++)
+        {
+            C[i, j] = A[i, 0] * B[0, j] + A[i, 1] * B[1, j];
+            Console.Write($"{C[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillMatrix(int[,] A, int rowsA, int colsA)
+{
+    for (int i = 0; i < rowsA; i++)
+        for (int j = 0; j < colsA; j++)
+        {
+            Console.Write($"Введите A({i},{j})");
+            A[i,j] = Convert.ToInt32(Console.ReadLine());
+        }
+    return A;
+}
+
+
+Console.Write("Введите количество строк матрицы А: ");
+int rowsA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы А: ");
+int colsA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк матрицы B: ");
+int rowsB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы В: ");
+int colsB = Convert.ToInt32(Console.ReadLine());
+
+int[,] A = new int[rowsA, colsA];
+int[,] B = new int[rowsB, colsB];
+FillMatrix(A, rowsA, colsA);
+FillMatrix(B, rowsB, colsB);
+MatrixProduct(A, B);
